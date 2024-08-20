d0c87ee582ecc44e0fef1360f995abda21469713
henryclyde <hen.clyde@gmail.com>
2024-08-19 18:18:10 -0600

    started html board

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
-let x = "x";
-let o = "o";
 
-function createPlayer(choice) {
+
+function createPlayer(name,choice) {
     let score = 0;
     const getScore = () => score;
     const giveScore = function() {
-        console.log(`Point for ${this.choice}!`);
-        return score++;
+        console.log(`Point for ${name}!`);
+        score++;
+        if(score === 3) {
+            player1.reset;
+            player2.reset; 
+            return `The winner is ${name}!`;           
+        }
     }
-    return {choice,getScore,giveScore};
+    function reset() {
+        score = 0;
+    }
+    return {choice,getScore,giveScore,reset};
 }
 
-const player1 = createPlayer(x);
-const player2 = createPlayer(o);
+const player1 = createPlayer("player1","x");
+const player2 = createPlayer("player2","o");
 
 function gameBoard() {
     let board = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
+    let topScore = 0;
+
     let winCombo = [
         [1,2,3],
         [4,5,6],
@@ -26,7 +35,6 @@ function gameBoard() {
         [1,5,9],
         [3,5,7]
     ]
-    let turn = 0; 
     function getBoard() {
         let viewBoard = board.map(x => x);
         viewBoard.splice(3,0,`\n`);
@@ -39,29 +47,46 @@ function gameBoard() {
     function checkWin() {
         for (let i in winCombo) {
             console.log(winCombo[i]);
-            if (board[winCombo[i][0]-1] == board[winCombo[i][1]-1] && board[winCombo[i][0]-1] == board[winCombo[i][2]-1]) {
-                if (board[winCombo[i][0]-1] === player1.choice) player1.giveScore();
-                else player2.giveScore();
-                board = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
-                break;
+            if (board[winCombo[i][0]-1] === board[winCombo[i][1]-1] && board[winCombo[i][0]-1] === board[winCombo[i][2]-1]) {
+                switch (board[winCombo[i][0]-1]) {
+                    case player1.choice: 
+                        player1.giveScore();
+                        board = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
+                        break;
+                    case player2.choice:
+                        player2.giveScore();
+                        board = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
+                        break;
+                    case ' ':
+                        continue;
+                }
             }
         }
     }
-    
-    return {board,turn,getBoard,setBoard,checkWin}
+    return {board,getBoard,setBoard,checkWin}
 }
 
 const main = gameBoard();
 
-main.setBoard(player2.choice,1);
-main.setBoard(player1.choice,2);
-main.setBoard(player2.choice,3);
-main.setBoard(player2.choice,4);
-main.setBoard(player1.choice,5);
-main.setBoard(player1.choice,6);
+
+
+main.setBoard(player1.choice,9);
 main.setBoard(player1.choice,7);
 main.setBoard(player1.choice,8);
-main.setBoard(player1.choice,9);
 
 console.log(main.getBoard());
 main.checkWin();
+
+
+
+main.setBoard(player1.choice,9);
+main.setBoard(player1.choice,7);
+main.setBoard(player1.choice,8);
+console.log(main.getBoard());
+
+main.checkWin();
+
+const game = document.querySelector(".gameboard");
+for (let i = 0; i < 9; i++) {
+    game.appendChild(document.createElement("div"));
+}
