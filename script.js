5c75246a49068f735c49fded1cb0972068aa131e
henryclyde <hen.clyde@gmail.com>
2024-08-22 02:03:15 -0600

    game now works, just need to fix reset after round

--- script.js
+++ script.js
@@ -18,12 +18,63 @@ function createPlayer(name,choice) {
     return {choice,getScore,giveScore,reset};
 }
 
-const player1 = createPlayer("player1","x");
-const player2 = createPlayer("player2","o");
 
 function gameBoard() {
     let board = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
     let topScore = 0;
+    let turnFirstPlayer = true;
+    let square = [];
+
+    
+    function buildBoard() {
+        board = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
+        square = [];
+        const game = document.querySelector(".gameboard");
+        let buttonX = document.getElementById("x");
+        let buttonO = document.getElementById("o");
+        player1 = createPlayer("player1",null);
+        player2 = createPlayer("player2",null); 
+
+        buttonX.addEventListener("click",function() {
+            player1.choice = buttonX.innerText;
+            player2.choice = buttonO.innerText;   
+            document.querySelector(".choice").style.visibility = "hidden";
+        });
+        
+        buttonO.addEventListener("click",function() {
+            player1.choice = buttonO.innerText;
+            player2.choice = buttonX.innerText;
+            document.querySelector(".choice").style.visibility = "hidden";
+        });
+        game.innerHTML = '';
+        for (let i = 0; i < 9; i++) {
+            square.push(document.createElement("div"));
+            game.appendChild(square[i]);
+            square[i].textContent = board[i];
+            // if (ready) {
+                document.querySelector(".turn").textContent = turnFirstPlayer == true ? "Player 1's turn!" : "Player 2's turn!";
+                square[i].addEventListener("click",function() {
+                    if (turnFirstPlayer) {
+                        setBoard(player1.choice,i+1);
+                        console.log(main.getBoard());
+                        checkWin();
+                        turnFirstPlayer = !turnFirstPlayer;
+                    }
+                    else {
+                        console.log(player2.choice)
+                        setBoard(player2.choice,i+1);
+                        console.log(main.getBoard());
+                        checkWin();
+                        turnFirstPlayer = !turnFirstPlayer;
+                    }
+                });
+            // }
+        }
+        return {square};
+    }
+    function play() {
+        buildBoard();
+    }
 
     let winCombo = [
         [1,2,3],
@@ -43,19 +94,23 @@ function gameBoard() {
     }
     function setBoard(player, pos) {
         board[pos-1] = player;
+        square[pos-1].textContent = player;
     }
     function checkWin() {
         for (let i in winCombo) {
-            console.log(winCombo[i]);
-            if (board[winCombo[i][0]-1] === board[winCombo[i][1]-1] && board[winCombo[i][0]-1] === board[winCombo[i][2]-1]) {
-                switch (board[winCombo[i][0]-1]) {
+            if (square[winCombo[i][0]-1].textContent === square[winCombo[i][1]-1].textContent && square[winCombo[i][0]-1].textContent === square[winCombo[i][2]-1].textContent) {
+                switch (square[winCombo[i][0]-1].textContent) {
                     case player1.choice: 
                         player1.giveScore();
+                        // buildBoard();
                         board = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
+                        square = [];
                         break;
                     case player2.choice:
                         player2.giveScore();
+                        // buildBoard();
                         board = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
+                        square = [];
                         break;
                     case ' ':
                         continue;
@@ -63,30 +118,20 @@ function gameBoard() {
             }
         }
     }
-    return {board,getBoard,setBoard,checkWin}
+    return {board,play,buildBoard,getBoard,setBoard,checkWin}
 }
 
 const main = gameBoard();
 
 
 
-main.setBoard(player1.choice,9);
-main.setBoard(player1.choice,7);
-main.setBoard(player1.choice,8);
-
-console.log(main.getBoard());
-main.checkWin();
+// main.setBoard(player1.choice,9);
+// main.setBoard(player1.choice,7);
+// main.setBoard(player1.choice,8);
 
 
 
-main.setBoard(player1.choice,9);
-main.setBoard(player1.choice,7);
-main.setBoard(player1.choice,8);
-console.log(main.getBoard());
 
-main.checkWin();
 
-const game = document.querySelector(".gameboard");
-for (let i = 0; i < 9; i++) {
-    game.appendChild(document.createElement("div"));
-}
+// main.buildBoard();
+main.play();
